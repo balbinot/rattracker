cd8495000b3b05ab3c09ae30e3d4d37db9a69fd5
Balbinot E  Dr (Physics) <e.balbinot@surrey.ac.uk>
2017-10-10 14:35:10 +0100

    serial read

--- monitor.py
+++ monitor.py
@@ -8,31 +8,38 @@ import threading
 
 ## Serial port simulation ##
 import os, pty, serial
-
-
-
-
-## END ##
-
-
 from Rat import Rat
 
 ## Define some Serial read functions
-queue = Queue.Queue(1000)
+queue = Queue.Queue(0)
 
 def serial_read(s):
     while True:
         line = s.readline()
         queue.put(line)
 
-serial0 = serial.Serial('/dev/ttyUSB0')
-serial1 = serial.Serial('/dev/ttyUSB1')
+# Define topology and port properties
+# -----------------------------------------------
+# |        |        |         |        |        |
+# |        P1       P2        P3       P4       |
+# |        |        |         |        |        |
+# -----------------------------------------------
 
-thread1 = threading.Thread(target=serial_read, args=(serial0,),).start()
-thread2 = threading.Thread(target=serial_read, args=(serial1,),).start()
+ports = {'P1': '/dev/ttyUSB0',
+         'P2': '/dev/ttyUSB1',
+         'P3': '/dev/ttyUSB2',
+         'P4': '/dev/ttyUSB3'}
 
+ports = {'P1': '/dev/pts/20'}
 
-#register rats
+# Register rats; ids should match whatever ID comes out of the RFID reader
 rat1 = Rat('Steve', '001')
 rat2 = Rat('Julia', '002')
 rat3 = Rat('Mario', '003')
+
+for dooraddr in ports.itervalues():
+    reader = serial.Serial(dooraddr, baudrate=9600)
+    thread = threading.Thread(target=serial_read, args=(reader,),).start()
+
+for a in queue:
+    print a
